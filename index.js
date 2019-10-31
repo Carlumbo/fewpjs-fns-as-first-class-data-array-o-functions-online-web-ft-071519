e7ee4a28eed334948ab6ccad79428b42429a3749
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-31 21:20:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var wakeDog = function(dogName="Byron", dogBreed="poodle") {
+let wakeDog = function(dogName="Byron", dogBreed="poodle") {
   console.log(`Wake ${dogName} the ${dogBreed}`);
   return `Wake ${dogName} the ${dogBreed}`
 }
