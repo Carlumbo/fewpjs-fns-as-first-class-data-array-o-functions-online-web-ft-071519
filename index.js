134162acb8e7ce60c14b4e06c9c82735293bf8ba
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-31 21:07:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function wakeDog(dogName, dogBreed) {
+  console.log(`wake ${dogName} the ${dogBreed}`); 
+}
+
+function leashDog(dogName,dogBreed) {
+  
+}
+
+function walkToPark(dogName, dogBreed) {
+
+}
+
+function throwFrisbee(dogName, dogBreed) {
+
+}
+
+function walkHom(dogName, dogBreed) {
+
+}
+
+function unleashDog(dogName, dogBreed) {
+
+}
+
+
+
+
+
